fccbe812e6606f6e2b66d72ed040adb3da280935
TAMWO FEUWO FRANCK VALERE <valeretamwo@gmail.com>
2022-06-22 22:20:55 +0100

    Mise de la vue

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Bonjour ')
+const express = require('express');
+const bodyparser = require('body-parser');
+const { render } = require('ejs');
+
+let app = express();
+const ejs = require('ejs');
+
+let port = 3000;
+app.set('view engine', 'ejs');
+
+//localhost : 3000
+
+app.get('/', (req, res) => {
+    //if (err) throw err
+    res.render('pages/accueil.ejs');
+})
+
+
+app.use('/assets', express.static('public'));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
